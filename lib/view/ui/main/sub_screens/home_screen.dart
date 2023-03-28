c37b8edf19719ec6828e91bb103019078bc3987c
segun <segun408@gmail.com>
2023-03-28 14:21:21 +0900

    keyboard control

--- lib/view/ui/main/sub_screens/home_screen.dart
+++ lib/view/ui/main/sub_screens/home_screen.dart
@@ -2,9 +2,9 @@ import 'package:chatai/controller/chat_controller.dart';
 import 'package:chatai/provider/chat_api.dart';
 import 'package:chatai/provider/firebase_api.dart';
 import 'package:chatai/repository/chat_repository.dart';
+import 'package:chatai/view/ui/main/widgets/chatList_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import '../widgets/chatList_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({
@@ -18,80 +18,83 @@ class HomeScreen extends StatelessWidget {
     final p = Get.find<FirebaseService>();
     p.getPreviousChats(p.roomNum.value);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('채팅'),
-        backgroundColor: const Color.fromRGBO(134, 93, 255, 1),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Obx(
-              () => ListView.builder(
-                reverse: true,
-                itemCount: p.chattingList.length,
-                itemBuilder: (context, index) {
-                  final reversedIndex = p.chattingList.length - index - 1;
-                  if (reversedIndex < 0) {
-                    return null;
-                  }
-                  final chattingModel = p.chattingList[reversedIndex];
-                  return ChattingItem(
-                    chattingModel: chattingModel,
-                  );
-                },
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('채팅'),
+          backgroundColor: const Color.fromRGBO(134, 93, 255, 1),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Obx(
+                () => ListView.builder(
+                  reverse: true,
+                  itemCount: p.chattingList.length,
+                  itemBuilder: (context, index) {
+                    final reversedIndex = p.chattingList.length - index - 1;
+                    if (reversedIndex < 0) {
+                      return null;
+                    }
+                    final chattingModel = p.chattingList[reversedIndex];
+                    return ChattingItem(
+                      chattingModel: chattingModel,
+                    );
+                  },
+                ),
               ),
             ),
-          ),
-          // 구분선
-          Divider(
-            thickness: 1.5,
-            height: 1.5,
-            color: Colors.grey[300],
-          ),
-          Container(
-            constraints: BoxConstraints(
-                maxHeight: MediaQuery.of(context).size.height * .5),
-            margin:
-                EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(15, 5, 15, 5),
-                    child: TextField(
-                      controller: p.sendController,
-                      maxLines: null,
-                      keyboardType: TextInputType.multiline,
-                      style: const TextStyle(fontSize: 27),
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: '텍스트 입력',
-                          hintStyle: TextStyle(color: Colors.grey[400])),
-                    ),
-                  ),
-                ),
-                GestureDetector(
-                  behavior: HitTestBehavior.opaque,
-                  onTap: () async {
-                    var text = p.sendController.text;
-                    var md = await Get.find<ChatController>().getAnswer(text);
-                    p.SendMessage(text, md);
-                    p.sendController.text = '';
-                    FocusScope.of(context).requestFocus(FocusNode());
-                  },
-                  child: const Padding(
-                    padding: EdgeInsets.fromLTRB(15, 5, 15, 5),
-                    child: Icon(
-                      Icons.send,
-                      size: 33,
-                    ),
-                  ),
-                )
-              ],
+            // 구분선
+            Divider(
+              thickness: 1.5,
+              height: 1.5,
+              color: Colors.grey[300],
             ),
-          )
-        ],
+            Container(
+              constraints: BoxConstraints(
+                  maxHeight: MediaQuery.of(context).size.height * .5),
+              margin: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).padding.bottom),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(15, 5, 15, 5),
+                      child: TextField(
+                        controller: p.sendController,
+                        maxLines: null,
+                        keyboardType: TextInputType.multiline,
+                        style: const TextStyle(fontSize: 27),
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: '텍스트 입력',
+                            hintStyle: TextStyle(color: Colors.grey[400])),
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    behavior: HitTestBehavior.opaque,
+                    onTap: () async {
+                      var text = p.sendController.text;
+                      var md = await Get.find<ChatController>().getAnswer(text);
+                      p.SendMessage(text, md);
+                      p.sendController.text = '';
+                      FocusScope.of(context).requestFocus(FocusNode());
+                    },
+                    child: const Padding(
+                      padding: EdgeInsets.fromLTRB(15, 5, 15, 5),
+                      child: Icon(
+                        Icons.send,
+                        size: 33,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
